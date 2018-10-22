3162244616bcb389266542a47b23aa8dacca0386
Vinchenso Kendall <chensok@gmail.com>
2018-10-22 17:04:47 +0200

    add sessions for auth

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 require("dotenv").config({ path: "variables.env" });
+const session = require("express-session");
 const createServer = require("./createServer.js");
 
 const server = createServer();
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
+server.express.use(
+  session({
+    name: "qid",
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+    }
+  })
+);
 
 server.start(
   {
