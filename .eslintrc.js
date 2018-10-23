90ec1ca665cdea52012516b833b0d9b45a1673a2
Vinchenso Kendall <chensok@gmail.com>
2018-10-23 14:06:16 +0200

    Linting Update - update eslint for graphql files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
+
 module.exports = {
   extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "graphql"],
   rules: {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+    "graphql/template-strings": ['error', {
+      env: 'literal',
+      schemaJson: '', //TODO- add schemaJSON
+    }]
   },
   env: {
     "jest": true
