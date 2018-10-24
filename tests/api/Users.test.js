155e45c9d06b002f12880f047e61150cd324dbdc
Vinchenso Kendall <chensok@gmail.com>
2018-10-24 06:16:19 +0200

    add unit test for guest

--- tests/api/Users.test.js
+++
@@ -1,9 +0,0 @@
-const db = require('../support/db.js');
-
-describe.only('User', () => {
-  beforeAll(db.connectToDB);
-  afterAll(db.disconnectDB);
-  afterEach(db.cleanDB);
-
-  test('Query', async () => {});
-});
---
+++ tests/api/guest.test.js
@@ -0,0 +1,18 @@
+const db = require('../support/db.js');
+const guestResolver = require('../../src/api/guest/guest.resolvers.js');
+
+describe.only('User', () => {
+  beforeAll(db.connectToDB);
+  afterAll(db.disconnectDB);
+  afterEach(db.cleanDB);
+
+  test('Query', async () => {
+    const guest = await db.models.guest.create({
+      firstname: 'test',
+      lastname: 'teser'
+    });
+    const result = await guestResolver.Query.allGuests(null, {}, db);
+
+    expect(`${result[0]._id}`).toBe(`${guest._id}`);
+  });
+});
